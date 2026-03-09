2fefc0792c1b0034ac65209fed21a8b5fec348d9
Antonio <antoma19@ucm.es>
2026-03-09 10:13:36 +0100

    Some refactoring for simplicity

--- try_kernel.py
+++ try_kernel.py
@@ -1,6 +1,7 @@
 import argparse
 
 
+from gem5.components.boards.x86_board import X86Board
 from gem5.components.cachehierarchies.classic.private_l1_shared_l2_cache_hierarchy import (
     PrivateL1SharedL2CacheHierarchy
 )
@@ -21,6 +22,8 @@ from pim import PIMAccelerator
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--no_acc", action="store_true", help="indicate to execute the kernel without accelerator")
+    parser.add_argument("--all_banks", action="store_true", help="indicate to execute the kernel that uses all banks")
     parser.add_argument("--kernel", help="kernel to execute")
     args = parser.parse_args()
     return args
@@ -43,6 +46,41 @@ def exit_handler():
     yield False
     yield True
 
+def exit_handler_all_banks():
+    bank_increment = 0x00004000 #We add two bank indexes, it's 4 and not 2 because banks bits take the last bit of the previous digit and the first 3 of the next -> 0000 0100
+    print("Exit event triggered - mapping memory region in the simulated process")
+    process = processor.get_cores()[0].core.workload[0]
+    # VA, PA, Size, Cacheable
+    process.map(0x10000000, 0xC4000000, 0x1000000, False) # PIM region
+    print("Mapped memory region at VA 0x10000000 to PA 0xC4000000")
+    yield False
+    region_size = 0x00010000
+    virtual_address = 0x20000000
+    physical_address = 0xD0000000
+    for i in range(8):
+        process.map(virtual_address, physical_address, region_size, False) # Operand A in bank 0
+        print("Mapped memory region at VA 0x{:x} to PA 0x{:x}".format(virtual_address, physical_address))
+        virtual_address += region_size
+        physical_address += bank_increment
+        yield False
+    virtual_address = 0x30000000
+    physical_address = 0xD0002000
+    for i in range(8):
+        process.map(virtual_address, physical_address, region_size, False) # Operand B in bank 1
+        print("Mapped memory region at VA 0x{:x} to PA 0x{:x}".format(virtual_address, physical_address))
+        virtual_address += region_size
+        physical_address += bank_increment
+        yield False
+    virtual_address = 0x40000000
+    physical_address = 0xD2000000
+    for i in range(8):
+        process.map(virtual_address, physical_address, region_size, False) # Operand C in bank 0
+        print("Mapped memory region at VA 0x{:x} to PA 0x{:x}".format(virtual_address, physical_address))
+        virtual_address += region_size
+        physical_address += bank_increment
+        yield False
+    yield True
+
 # Here we setup a no-cache hierarchy to better observe the PIM behavior without cache effects.
 #cache_hierarchy = NoCache()
 
@@ -62,23 +100,50 @@ memory = SingleChannelDDR4_2400(size="3GB")
 
 processor = SimpleProcessor(num_cores=1,isa=ISA.X86,cpu_type=CPUTypes.TIMING)
 
-pim = PIMAccelerator(size="3GB")
-
-board = PIMBoard(
-    clk_freq="1GHz",
-    processor=processor,
-    memory=memory,
-    cache_hierarchy=cache_hierarchy,
-    pim=pim,
-)
-
 kernels_path = "/homelocal/antoma19_local/u/tfm/pim-resources/binaries/"
 
+board = None
+
+if args.no_acc:
+    print("Running without accelerator")
+    pim = None
+    board = X86Board(
+        clk_freq="1GHz",
+        processor=processor,
+        memory=memory,
+        cache_hierarchy=cache_hierarchy,
+    )
+    kernels_path += "no_acc/"
+else:
+    pim = PIMAccelerator(size="3GB")
+    board = PIMBoard(
+        clk_freq="1GHz",
+        processor=processor,
+        memory=memory,
+        cache_hierarchy=cache_hierarchy,
+        pim=pim,
+    )
+    if args.all_banks:
+        print("Running kernel that uses all banks")
+        kernels_path += "all_banks/"
+
+
 board.set_se_binary_workload(BinaryResource(kernels_path + args.kernel))
-simulator = Simulator(
-    board=board,
-    on_exit_event= {
-        ExitEvent.EXIT: exit_handler(),
-    }
-)
+
+simulator = None
+
+handler = exit_handler_all_banks() if args.all_banks else exit_handler()
+
+if args.no_acc:
+    simulator = Simulator(
+        board=board,
+    )
+elif args.all_banks:
+    simulator = Simulator(
+        board=board,
+        on_exit_event= {
+            ExitEvent.EXIT: handler,
+        }
+    )
+    
 simulator.run()
